958098bb100bfa6b22cb49604669fa460aed1436
猪股浩平 <>
2022-04-14 22:28:21 +0900

    light sensor mqtt app ・照度センサー値取得処理追加

--- app/src/main/java/com/example/lightsensor_mqtt_app/MainActivity.java
+++ app/src/main/java/com/example/lightsensor_mqtt_app/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.lightsensor_mqtt_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mLight;
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // センサーオブジェクトを取得
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        // 照度センサーのオブジェクトを取得
+        mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        // センサーのリスナー登録
+        mSensorManager.registerListener(this,mLight,SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // 近接センサーを無効
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_LIGHT){
+            Log.i(TAG,"Light!! value:"+event.values[0]);
+            TextView textView = (TextView)findViewById(R.id.lightValueText);
+            textView.setText(String.valueOf(event.values[0]));
+        }
+
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // センサーの精度を変更するときに使う
+    }
+
 }
