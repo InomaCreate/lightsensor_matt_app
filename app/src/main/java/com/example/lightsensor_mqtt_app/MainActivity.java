b0038e8d88a1e52935d4e071c16c105f57a0e149
猪股浩平 <>
2022-04-16 18:55:41 +0900

    LightSensor Mqtt App ・MQTTブローカー、パブリッシュ処理追加

--- app/src/main/java/com/example/lightsensor_mqtt_app/MainActivity.java
+++ app/src/main/java/com/example/lightsensor_mqtt_app/MainActivity.java
@@ -3,24 +3,129 @@ package com.example.lightsensor_mqtt_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.TextView;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import io.moquette.BrokerConstants;
+import io.moquette.broker.config.MemoryConfig;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
     private Sensor mLight;
     private String TAG = "MainActivity";
+    io.moquette.broker.Server broker;
+    String IPAddress=null;
+    private MqttAndroidClient mqttAndroidClient;
+    private int LightValue;
+    private Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(mqttAndroidClient == null)
+                    return;
+
+                try {
+                    Log.i(TAG,"publish!!");
+                    mqttAndroidClient.publish("sensor/light", String.valueOf(LightValue).getBytes(), 0, true);
+                } catch (MqttPersistenceException e) {
+                    Log.d(TAG,e.toString());
+                } catch (MqttException e) {
+                    Log.d(TAG,e.toString());
+                }
+
+            }
+        },1000,1000);
+
+        // Broker起動処理
+        try {
+            broker = new io.moquette.broker.Server();
+
+            MemoryConfig memoryConfig = new MemoryConfig(new Properties());
+            memoryConfig.setProperty(BrokerConstants.PERSISTENT_STORE_PROPERTY_NAME, this.getFilesDir() + BrokerConstants.DEFAULT_MOQUETTE_STORE_H2_DB_FILENAME);
+
+            broker.startServer(memoryConfig);
+            Log.i(TAG,"startServer");
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        // 自分のIPアドレスを取得する
+        try {
+            for (NetworkInterface n : Collections.list(NetworkInterface.getNetworkInterfaces())) {
+                for (InetAddress addr : Collections.list(n.getInetAddresses())) {
+                    if(addr instanceof Inet4Address && !addr.isLoopbackAddress()) {
+                        Log.i(TAG,"IP ADDRESS:"+addr.getHostAddress());
+                        IPAddress = addr.getHostAddress();
+                        TextView textView = (TextView)findViewById(R.id.textIP);
+                        textView.setText(IPAddress);
+                    }
+                }
+            }
+        } catch (SocketException e){
+            Log.e(TAG,"IP address not get!!");
+            e.printStackTrace();
+        }
+
+        // クライアントブローカー接続
+        if(IPAddress!=null) {
+            mqttAndroidClient = new MqttAndroidClient(this, "tcp://" + IPAddress + ":1883", "test-android") {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    super.onReceive(context, intent);
+                }
+            };
+            try {
+                MqttConnectOptions options = new MqttConnectOptions();
+                mqttAndroidClient.connect(options, null, new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken iMqttToken) {
+                        Log.d(TAG, "onSuccess");
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
+                        Log.d(TAG, "onFailure");
+                    }
+                });
+
+            } catch (MqttException e) {
+                Log.d(TAG, e.toString());
+            }
+        }
+
 
         // センサーオブジェクトを取得
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -42,7 +147,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onPause() {
         super.onPause();
-        // 近接センサーを無効
+        // センサーを無効
         mSensorManager.unregisterListener(this);
     }
 
@@ -52,6 +157,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Log.i(TAG,"Light!! value:"+event.values[0]);
             TextView textView = (TextView)findViewById(R.id.lightValueText);
             textView.setText(String.valueOf(event.values[0]));
+            LightValue = (int)event.values[0];
         }
 
     }
